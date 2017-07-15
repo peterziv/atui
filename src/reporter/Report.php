5c38814ed2fad8742a9cd44947feb90a879837eb
peterziv <peter.ziv@hotmail.com>
2017-07-15 23:53:08 +0800

    Add comment

--- src/reporter/Report.php
+++ src/reporter/Report.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * ATUI
+ * @author Peter (peter.ziv@hotmail.com)
+ * @date July 15, 2017
+ * @version 1.0.0
+ */
+
 require_once(__DIR__ . '/HandleJUnit.php');
 
 //define('APP_DEBUG', true);
