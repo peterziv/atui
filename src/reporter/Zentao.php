5b6ea7a4ba7589420c5ced50e25c3cb76672b624
peterziv <peter.ziv@hotmail.com>
2017-07-15 23:52:19 +0800

    Add bug check at first, it will be bug edit when there's one same title bug.

--- src/reporter/Zentao.php
+++ src/reporter/Zentao.php
@@ -22,7 +22,7 @@ namespace ZKit\ATUI {
             if ($this->login($sid)) {
                 $this->bug($sid, $data);
             } else {
-                echo "[ERORR] Failed to login in the Bug Management system(ZenTao)";
+                $this->log->error('Failed to login in the Bug Management system(ZenTao)');
             }
         }
 
@@ -46,18 +46,54 @@ namespace ZKit\ATUI {
 
         private function bug($sid, $data)
         {
-            $product = $data['product'];
-            $this->client->post($this->domain . '/index.php?m=bug&f=create&t=json&productID=' .
-                $product . '&' . $sid, $data);
+            $bug = $this->findBugByTitle($sid, $data);
+            if (!is_null($bug)) {
+                $bug['steps'] .= '<p>' . date('Y-m-d H:i:s') . 'Reported the same bug via ATUI</p>';
+                $this->editBug($sid, $bug);
+            } else {
+                $this->createBug($sid, $data);
+            }
         }
 
-        private function findBugByTitle($title)
+        private function createBug($sid, $data)
         {
-            $product = $data['product'];
+            $this->log->info('Report the bug!');
+            $data['steps'] = '<p>' . date('Y-m-d H:i:s') . 'Reported the bug via ATUI</p>';
             $this->client->post($this->domain . '/index.php?m=bug&f=create&t=json&productID=' .
-                $product . '&' . $sid, $data);
+                $data['product'] . '&' . $sid, $data);
         }
 
-    }
+        private function editBug($sid, $data)
+        {
+            $this->log->debug($data);
+            $this->log->warning('This bug is reported before! #' . $data['id']);
+            $v = $this->client->post($this->domain . '/index.php?m=bug&f=edit&t=json&bugID=' .
+                $data['id'] . '&' . $sid, $data);
+            $this->log->debug($v);
+        }
 
+        private function findBugByTitle($sid, $data = array())
+        {
+            $rs = $this->client->get($this->domain . '/index.php?m=bug&f=browse&t=json&productID=' .
+                $data['product'] . '&' . $sid);
+            $result = $this->client->parse($rs);
+            $sameBug = null;
+            foreach ($result['bugs'] as $bug) {
+                $this->log->debug($bug['id'] . ' ' . $bug['title']);
+                if ($bug['title'] != htmlspecialchars($data['title'])) {
+                    continue;
+                }
+
+                //TODO there's one bug for lanuage issue.
+//                if (strpos($bug['openedBuild'], $data['openedBuild']) === false) {
+//                    $data['openedBuild'] .= ',' . $bug['openedBuild'];
+//                }
+                $data['id'] = $bug['id'];
+                $data['steps'] = $bug['steps'];
+                $sameBug = $data;
+                break;
+            }
+            return $sameBug;
+        }
+    }
 }
