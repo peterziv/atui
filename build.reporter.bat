c7c373223d4bd94af6679b97911c1d124af8bc6f
peterziv <peter.ziv@hotmail.com>
2017-07-24 21:02:12 +0800

    move the build script under root path

--- build.reporter.bat
+++ build.reporter.bat
@@ -6,19 +6,20 @@ echo Author: peter^<peter.ziv@hotmail.com^>
 echo Date: July 15,2017
 echo -----------------------------------
 
+set root=%~dp0
 set app=reporter
 set version=1.0.alpha2
 
 set package=%app%-%version%.phar
-php zphar-1.0.1.phar --dir reporter --name %package% --default Report.php
+php tools\zphar-1.0.1.phar --dir src\reporter --name %package% --default Report.php
 
-echo [INFO] Waiting for some time
+echo [INFO] Waiting for some time to repare application...
 @ping -n 2 127.0.0.1 >nul
 
-move /y %package% ..\deploy\bin
+move /y %package% deploy\bin
 
 echo [INFO] Testing...
-cd ..\deploy
+cd deploy
 php bin\%package%
 
-cd ..\src
+cd %root%
